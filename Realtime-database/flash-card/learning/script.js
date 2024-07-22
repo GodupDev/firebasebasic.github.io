13d7cb56672dcc883c6456fc450630c3238c3287
GodupDev <157476074+GodupDev@users.noreply.github.com>
2024-07-22 10:59:50 +0700

    Add files via upload
    firstCommit

---
+++ Realtime-database/flash-card/learning/script.js
@@ -0,0 +1,160 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+import {
+  getDatabase,
+  set,
+  ref,
+  update,
+  onValue,
+  get,
+  remove,
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB5X99chT6OvJ2rDLfqFL1MrKh8uMNMFa8",
+  authDomain: "learn-firebase-52523.firebaseapp.com",
+  databaseURL: "https://learn-firebase-52523-default-rtdb.firebaseio.com",
+  projectId: "learn-firebase-52523",
+  storageBucket: "learn-firebase-52523.appspot.com",
+  messagingSenderId: "561012229632",
+  appId: "1:561012229632:web:be2ddd92496633d009bae5",
+  measurementId: "G-PLLLT1QC70",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase(app);
+
+// Array to store the added words and their meanings
+
+const randint = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var Score = 0;
+var check = 0;
+var randword = [];
+var randomIndex = 0;
+
+const btnCheck = document.getElementById("submit");
+
+const getData = async () => {
+  try {
+    const starCountRef = ref(db, "FlashCard");
+    const snapshot = await get(starCountRef);
+    console.log("Data got Subccessfully");
+    return snapshot.val();
+  } catch (error) {
+    console.log("Error getting data:", error.message);
+  }
+};
+
+const loadData = async () => {
+  const data = await getData();
+  var wordList = [];
+  for (let [key, value] of Object.entries(data)) {
+    const id = key;
+    const word = value.word;
+    const partOfSpeech = value.partOfSpeech;
+    const meaning = value.meaning;
+    wordList.push({ id, word, partOfSpeech, meaning });
+  }
+  return wordList;
+};
+
+var wordList = await loadData();
+const size = wordList.length;
+console.log(wordList);
+
+const updateCard = (randWord) => {
+  const flip_card_front = document.getElementById("flip-card-front");
+  const flip_card_back = document.getElementById("flip-card-back");
+  const inputAns = document.getElementById("inputAns");
+  randword = randWord;
+  check = randint(0, 1);
+  if (check) {
+    flip_card_front.innerHTML = `<p class="title">${randWord.word}</p> <p>${randWord.partOfSpeech}</p>`;
+    flip_card_back.innerHTML = `<p class="title">${randWord.meaning}</p>`;
+    inputAns.innerHTML = `<input type="text" placeholder="Nhập nghĩa của từ trên thẻ" id="answer">`;
+  } else {
+    flip_card_front.innerHTML = `<p class="title">${randWord.meaning}</p> <p>${randWord.partOfSpeech}</p>`;
+    flip_card_back.innerHTML = `<p class="title">${randWord.word}</p>`;
+    inputAns.innerHTML = `<input type="text" placeholder="Nhập từ có nghĩa trên thẻ" id="answer">`;
+  }
+};
+
+const randWord = () => {
+  const randomIndex = randint(0, wordList.length - 1);
+  updateCard(wordList[randomIndex]);
+  return randomIndex;
+};
+
+randomIndex = randWord();
+
+const CheckAns = () => {
+  const answerInput = document.getElementById("answer").value;
+  const { word, meaning } = randword;
+  const meanings = meaning.split(",").map((m) => m.trim().toLowerCase());
+  const scoreValue = document.getElementById("score-value");
+  const scoreProgress = document.getElementById("score-progress");
+  const flip_card_back = document.getElementById("flip-card-back");
+  const flip_card_inner = document.getElementById("flip-card-inner");
+
+  let isCorrect = false;
+
+  if (check) {
+    if (meanings.includes(answerInput.toLowerCase())) {
+      isCorrect = true;
+    }
+  } else {
+    if (answerInput.toLowerCase() === word.toLowerCase()) {
+      isCorrect = true;
+    }
+  }
+
+  if (isCorrect) {
+    Score += 1;
+    scoreProgress.style.width = `${(Score / size) * 100}%`;
+    scoreValue.innerHTML = Score;
+    wordList.splice(randomIndex, 1);
+    flip_card_back.style.backgroundColor = `green`;
+    flip_card_inner.style.transform = `rotateY(180deg)`;
+    flip_card_back.addEventListener("click", () => {
+      flip_card_back.style.visibility = 'hidden';
+      flip_card_inner.style.transform = `rotateY(0deg)`;
+      randomIndex = randWord();
+      setTimeout(() => {
+      flip_card_back.style.visibility = 'visible';
+      }, 800);
+    });
+    setTimeout(() => {
+      if (Score == size) {
+        const continueLearn = confirm("Bạn đã học hết các từ vựng có muốn học lại hay không");
+        if (continueLearn) location.reload();
+        else window.location = "../home/index.html";
+      }
+      }, 2000);
+  } else {
+    scoreProgress.style.color = `red`;
+    flip_card_back.innerHTML += `<p><del>${answerInput}</del></p>`;
+    flip_card_back.style.backgroundColor = `red`;
+    flip_card_inner.style.transform = `rotateY(180deg)`;
+
+    flip_card_back.addEventListener("click", () => {
+      flip_card_back.style.visibility = 'hidden';
+      flip_card_inner.style.transform = `rotateY(0deg)`;
+      randomIndex = randWord();
+      setTimeout(() => {
+      flip_card_back.style.visibility = 'visible';
+      }, 800);
+    });
+  }
+};
+
+btnCheck.addEventListener("click", CheckAns);
